6ea1c1a28ba68ad9125e7af64eaf6af215d8f7bb
bomberoscapitalsanjuan <136654074+bomberoscapitalsanjuan@users.noreply.github.com>
2023-06-16 18:53:05 -0300

    bomberos

---
+++ BOMBEROS/Form1.Designer.vb
@@ -0,0 +1,468 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As ComponentModel.ComponentResourceManager = New ComponentModel.ComponentResourceManager(GetType(Form1))
+        Panel1 = New Panel()
+        buttconfiguracion = New Button()
+        buttaspirantes = New Button()
+        butttelefonos = New Button()
+        buttcumplimiento = New Button()
+        buttdatos = New Button()
+        PictureBox1 = New PictureBox()
+        Label2 = New Label()
+        Label1 = New Label()
+        GroupBox1 = New GroupBox()
+        Panel2 = New Panel()
+        Button1 = New Button()
+        Button6 = New Button()
+        Label3 = New Label()
+        Panel3 = New Panel()
+        buttcargar = New Button()
+        Label5 = New Label()
+        Label4 = New Label()
+        Panel4 = New Panel()
+        txt_aut = New Label()
+        Label9 = New Label()
+        Button8 = New Button()
+        box_contr = New TextBox()
+        box_usuario = New TextBox()
+        Label8 = New Label()
+        Label7 = New Label()
+        Label6 = New Label()
+        PictureBox2 = New PictureBox()
+        textoo = New Label()
+        Panel1.SuspendLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        Panel2.SuspendLayout()
+        Panel3.SuspendLayout()
+        Panel4.SuspendLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.BackColor = Color.Transparent
+        Panel1.BackgroundImage = CType(resources.GetObject("Panel1.BackgroundImage"), Image)
+        Panel1.Controls.Add(buttconfiguracion)
+        Panel1.Controls.Add(buttaspirantes)
+        Panel1.Controls.Add(butttelefonos)
+        Panel1.Controls.Add(buttcumplimiento)
+        Panel1.Controls.Add(buttdatos)
+        Panel1.Controls.Add(PictureBox1)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(GroupBox1)
+        Panel1.Dock = DockStyle.Left
+        Panel1.Location = New Point(0, 0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(280, 611)
+        Panel1.TabIndex = 0
+        ' 
+        ' buttconfiguracion
+        ' 
+        buttconfiguracion.BackColor = Color.Transparent
+        buttconfiguracion.Enabled = False
+        buttconfiguracion.FlatAppearance.BorderColor = Color.Maroon
+        buttconfiguracion.FlatAppearance.BorderSize = 0
+        buttconfiguracion.FlatAppearance.MouseDownBackColor = Color.Brown
+        buttconfiguracion.FlatAppearance.MouseOverBackColor = Color.IndianRed
+        buttconfiguracion.FlatStyle = FlatStyle.Flat
+        buttconfiguracion.Font = New Font("Microsoft Sans Serif", 12.75F, FontStyle.Bold, GraphicsUnit.Point)
+        buttconfiguracion.ForeColor = Color.White
+        buttconfiguracion.Location = New Point(3, 549)
+        buttconfiguracion.Name = "buttconfiguracion"
+        buttconfiguracion.Size = New Size(280, 50)
+        buttconfiguracion.TabIndex = 8
+        buttconfiguracion.Text = "CONFIGURACION"
+        buttconfiguracion.UseVisualStyleBackColor = False
+        ' 
+        ' buttaspirantes
+        ' 
+        buttaspirantes.BackColor = Color.Transparent
+        buttaspirantes.Enabled = False
+        buttaspirantes.FlatAppearance.BorderColor = Color.Maroon
+        buttaspirantes.FlatAppearance.BorderSize = 0
+        buttaspirantes.FlatAppearance.MouseDownBackColor = Color.Brown
+        buttaspirantes.FlatAppearance.MouseOverBackColor = Color.IndianRed
+        buttaspirantes.FlatStyle = FlatStyle.Flat
+        buttaspirantes.Font = New Font("Microsoft Sans Serif", 12.75F, FontStyle.Bold, GraphicsUnit.Point)
+        buttaspirantes.ForeColor = Color.White
+        buttaspirantes.Location = New Point(0, 367)
+        buttaspirantes.Name = "buttaspirantes"
+        buttaspirantes.Size = New Size(280, 50)
+        buttaspirantes.TabIndex = 7
+        buttaspirantes.Text = "ASPIRANTES"
+        buttaspirantes.UseVisualStyleBackColor = False
+        ' 
+        ' butttelefonos
+        ' 
+        butttelefonos.BackColor = Color.Transparent
+        butttelefonos.Enabled = False
+        butttelefonos.FlatAppearance.BorderColor = Color.Maroon
+        butttelefonos.FlatAppearance.BorderSize = 0
+        butttelefonos.FlatAppearance.MouseDownBackColor = Color.Brown
+        butttelefonos.FlatAppearance.MouseOverBackColor = Color.IndianRed
+        butttelefonos.FlatStyle = FlatStyle.Flat
+        butttelefonos.Font = New Font("Microsoft Sans Serif", 12.75F, FontStyle.Bold, GraphicsUnit.Point)
+        butttelefonos.ForeColor = Color.White
+        butttelefonos.Location = New Point(0, 285)
+        butttelefonos.Name = "butttelefonos"
+        butttelefonos.Size = New Size(280, 50)
+        butttelefonos.TabIndex = 6
+        butttelefonos.Text = "TELEFONOS UTILES"
+        butttelefonos.UseVisualStyleBackColor = False
+        ' 
+        ' buttcumplimiento
+        ' 
+        buttcumplimiento.BackColor = Color.Transparent
+        buttcumplimiento.Enabled = False
+        buttcumplimiento.FlatAppearance.BorderColor = Color.Maroon
+        buttcumplimiento.FlatAppearance.BorderSize = 0
+        buttcumplimiento.FlatAppearance.MouseDownBackColor = Color.Brown
+        buttcumplimiento.FlatAppearance.MouseOverBackColor = Color.IndianRed
+        buttcumplimiento.FlatStyle = FlatStyle.Flat
+        buttcumplimiento.Font = New Font("Microsoft Sans Serif", 12.75F, FontStyle.Bold, GraphicsUnit.Point)
+        buttcumplimiento.ForeColor = Color.White
+        buttcumplimiento.Location = New Point(0, 229)
+        buttcumplimiento.Name = "buttcumplimiento"
+        buttcumplimiento.Size = New Size(280, 50)
+        buttcumplimiento.TabIndex = 5
+        buttcumplimiento.Text = "CUMPLIMIENTO"
+        buttcumplimiento.UseVisualStyleBackColor = False
+        ' 
+        ' buttdatos
+        ' 
+        buttdatos.BackColor = Color.Transparent
+        buttdatos.Enabled = False
+        buttdatos.FlatAppearance.BorderColor = Color.Maroon
+        buttdatos.FlatAppearance.BorderSize = 0
+        buttdatos.FlatAppearance.MouseDownBackColor = Color.Brown
+        buttdatos.FlatAppearance.MouseOverBackColor = Color.IndianRed
+        buttdatos.FlatStyle = FlatStyle.Flat
+        buttdatos.Font = New Font("Microsoft Sans Serif", 12.75F, FontStyle.Bold, GraphicsUnit.Point)
+        buttdatos.ForeColor = Color.White
+        buttdatos.Location = New Point(0, 173)
+        buttdatos.Name = "buttdatos"
+        buttdatos.Size = New Size(280, 50)
+        buttdatos.TabIndex = 4
+        buttdatos.Text = "DATOS BOMBEROS"
+        buttdatos.UseVisualStyleBackColor = False
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(94, 9)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(77, 106)
+        PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        PictureBox1.TabIndex = 3
+        PictureBox1.TabStop = False
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial", 8.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.ForeColor = SystemColors.ActiveCaption
+        Label2.Location = New Point(98, 137)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(70, 14)
+        Label2.TabIndex = 2
+        Label2.Text = "DE CAPITAL"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ControlLight
+        Label1.Location = New Point(50, 121)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(179, 16)
+        Label1.TabIndex = 1
+        Label1.Text = "BOMBEROS VOLUNTARIOS"
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Location = New Point(-6, 151)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(286, 11)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        ' 
+        ' Panel2
+        ' 
+        Panel2.BackColor = Color.Brown
+        Panel2.Controls.Add(Button1)
+        Panel2.Controls.Add(Button6)
+        Panel2.Controls.Add(Label3)
+        Panel2.Dock = DockStyle.Top
+        Panel2.Location = New Point(280, 0)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(904, 55)
+        Panel2.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.FlatAppearance.BorderSize = 0
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Segoe UI", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Button1.ForeColor = Color.White
+        Button1.Location = New Point(816, 12)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(35, 33)
+        Button1.TabIndex = 2
+        Button1.Text = "-"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.FlatAppearance.BorderSize = 0
+        Button6.FlatStyle = FlatStyle.Flat
+        Button6.Font = New Font("Segoe UI", 9.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Button6.ForeColor = Color.White
+        Button6.Location = New Point(857, 12)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(35, 33)
+        Button6.TabIndex = 1
+        Button6.Text = "x"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial Black", 15.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.ForeColor = Color.FromArgb(CByte(224), CByte(224), CByte(224))
+        Label3.Location = New Point(231, 14)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(450, 28)
+        Label3.TabIndex = 0
+        Label3.Text = "BOMBEROS VOLUNTARIOS DE CAPITAL"
+        ' 
+        ' Panel3
+        ' 
+        Panel3.BackColor = Color.Salmon
+        Panel3.Controls.Add(buttcargar)
+        Panel3.Controls.Add(Label5)
+        Panel3.Controls.Add(Label4)
+        Panel3.Dock = DockStyle.Bottom
+        Panel3.Location = New Point(280, 572)
+        Panel3.Name = "Panel3"
+        Panel3.Size = New Size(904, 39)
+        Panel3.TabIndex = 2
+        ' 
+        ' buttcargar
+        ' 
+        buttcargar.Enabled = False
+        buttcargar.FlatStyle = FlatStyle.Flat
+        buttcargar.Location = New Point(767, 3)
+        buttcargar.Name = "buttcargar"
+        buttcargar.Size = New Size(125, 33)
+        buttcargar.TabIndex = 2
+        buttcargar.Text = "CARGAR"
+        buttcargar.UseVisualStyleBackColor = True
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(165, 12)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(71, 15)
+        Label5.TabIndex = 1
+        Label5.Text = "DD/MM/AA"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(22, 12)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(139, 15)
+        Label4.TabIndex = 0
+        Label4.Text = "ULTIMA MODIFICACION:"
+        ' 
+        ' Panel4
+        ' 
+        Panel4.BackColor = Color.FromArgb(CByte(255), CByte(208), CByte(209))
+        Panel4.Controls.Add(textoo)
+        Panel4.Controls.Add(txt_aut)
+        Panel4.Controls.Add(Label9)
+        Panel4.Controls.Add(Button8)
+        Panel4.Controls.Add(box_contr)
+        Panel4.Controls.Add(box_usuario)
+        Panel4.Controls.Add(Label8)
+        Panel4.Controls.Add(Label7)
+        Panel4.Controls.Add(Label6)
+        Panel4.Controls.Add(PictureBox2)
+        Panel4.Dock = DockStyle.Fill
+        Panel4.Location = New Point(280, 55)
+        Panel4.Name = "Panel4"
+        Panel4.Size = New Size(904, 517)
+        Panel4.TabIndex = 3
+        ' 
+        ' txt_aut
+        ' 
+        txt_aut.AutoSize = True
+        txt_aut.Location = New Point(713, 420)
+        txt_aut.Name = "txt_aut"
+        txt_aut.Size = New Size(47, 15)
+        txt_aut.TabIndex = 8
+        txt_aut.Text = "Label10"
+        txt_aut.Visible = False
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(631, 420)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(77, 15)
+        Label9.TabIndex = 7
+        Label9.Text = "Autorización:"
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(509, 411)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(92, 33)
+        Button8.TabIndex = 6
+        Button8.Text = "Ingresar"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' box_contr
+        ' 
+        box_contr.Location = New Point(232, 434)
+        box_contr.Name = "box_contr"
+        box_contr.Size = New Size(256, 23)
+        box_contr.TabIndex = 5
+        ' 
+        ' box_usuario
+        ' 
+        box_usuario.Location = New Point(232, 398)
+        box_usuario.Name = "box_usuario"
+        box_usuario.Size = New Size(256, 23)
+        box_usuario.TabIndex = 4
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(112, 435)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(94, 18)
+        Label8.TabIndex = 3
+        Label8.Text = "Contraseña:"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Arial", 11.25F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(112, 403)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(67, 18)
+        Label7.TabIndex = 2
+        Label7.Text = "Usuario:"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Arial Black", 18.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(327, 27)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(275, 33)
+        Label6.TabIndex = 1
+        Label6.Text = "GESTION DE DATOS"
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), Image)
+        PictureBox2.Location = New Point(368, 63)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(183, 276)
+        PictureBox2.SizeMode = PictureBoxSizeMode.AutoSize
+        PictureBox2.TabIndex = 0
+        PictureBox2.TabStop = False
+        ' 
+        ' textoo
+        ' 
+        textoo.AutoSize = True
+        textoo.Location = New Point(658, 224)
+        textoo.Name = "textoo"
+        textoo.Size = New Size(47, 15)
+        textoo.TabIndex = 9
+        textoo.Text = "Label10"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7.0F, 15.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1184, 611)
+        Controls.Add(Panel4)
+        Controls.Add(Panel3)
+        Controls.Add(Panel2)
+        Controls.Add(Panel1)
+        FormBorderStyle = FormBorderStyle.None
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        Panel2.ResumeLayout(False)
+        Panel2.PerformLayout()
+        Panel3.ResumeLayout(False)
+        Panel3.PerformLayout()
+        Panel4.ResumeLayout(False)
+        Panel4.PerformLayout()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents buttdatos As Button
+    Friend WithEvents buttconfiguracion As Button
+    Friend WithEvents buttaspirantes As Button
+    Friend WithEvents butttelefonos As Button
+    Friend WithEvents buttcumplimiento As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents buttcargar As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button8 As Button
+    Friend WithEvents box_contr As TextBox
+    Friend WithEvents box_usuario As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txt_aut As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents textoo As Label
+End Class
---
+++ BOMBEROS/Form1.vb
@@ -0,0 +1,69 @@
+﻿
+Imports System.IO
+
+Public Class Form1
+    Private archivo As StreamReader
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If box_usuario.Text = "bombero" And box_contr.Text = "bombero" Then
+            buttdatos.Enabled = True
+            buttcumplimiento.Enabled = True
+            butttelefonos.Enabled = True
+            buttaspirantes.Enabled = False
+            buttconfiguracion.Enabled = False
+            buttcargar.Enabled = False
+            txt_aut.Visible = True
+            txt_aut.Text = "BOMBERO"
+            txt_aut.ForeColor = Color.Blue
+        ElseIf box_usuario.Text = "instructor" And box_contr.Text = "instructor" Then
+            buttdatos.Enabled = True
+            buttcumplimiento.Enabled = True
+            butttelefonos.Enabled = True
+            buttaspirantes.Enabled = True
+            buttconfiguracion.Enabled = False
+            buttcargar.Enabled = False
+            txt_aut.Visible = True
+            txt_aut.Text = "INSTRUCTOR"
+            txt_aut.ForeColor = Color.Green
+        ElseIf box_usuario.Text = "admin" And box_contr.Text = "admin" Then
+            buttdatos.Enabled = True
+            buttcumplimiento.Enabled = True
+            butttelefonos.Enabled = True
+            buttaspirantes.Enabled = True
+            buttconfiguracion.Enabled = True
+            buttcargar.Enabled = True
+            txt_aut.Visible = True
+            txt_aut.Text = "ADMINISTRADOR"
+            txt_aut.ForeColor = Color.Red
+        Else
+            buttdatos.Enabled = False
+            buttcumplimiento.Enabled = False
+            butttelefonos.Enabled = False
+            buttaspirantes.Enabled = False
+            buttconfiguracion.Enabled = False
+            buttcargar.Enabled = False
+            txt_aut.Visible = True
+            txt_aut.Text = "Incorrecto"
+            txt_aut.ForeColor = Color.Black
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub buttcargar_Click(sender As Object, e As EventArgs) Handles buttcargar.Click
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText("d:\Users\pc\Documents\nuevo programa cuartel\base de datos\BOMBEROS\basedatos.txt")
+        textoo.Text = fileReader
+    End Sub
+End Class
